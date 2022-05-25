bb7494e5e786deb4e24673b0453d6a3d49b58136
Michal Cichra <michal@cichra.cz>
2022-05-25 13:36:54 +0200

    fix null reference exceptions

--- Editor/Utilities/SceneHierarchyUtility.cs
+++ Editor/Utilities/SceneHierarchyUtility.cs
@@ -1,7 +1,9 @@
 ﻿// Partially from: https://github.com/sandolkakos/unity-utilities/blob/main/Scripts/Editor/SceneHierarchyUtility.cs
 
+using System;
 using System.Collections.Generic;
 using System.Reflection;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,6 +15,10 @@ namespace SaG.MainSceneAutoLoading.Utilities
     /// </summary>
     public static class SceneHierarchyUtility
     {
+        [NotNull]
+        private static Type SceneHierarchyWindowType => typeof(EditorWindow).Assembly
+            .GetType("UnityEditor.SceneHierarchyWindow");
+
         /// <summary>
         /// Check if the target GameObject is expanded (aka unfolded) in the Hierarchy view.
         /// </summary>
@@ -26,13 +32,15 @@ namespace SaG.MainSceneAutoLoading.Utilities
         /// </summary>
         public static List<GameObject> GetExpandedGameObjects()
         {
-            object sceneHierarchy = GetSceneHierarchy();
+            var sceneHierarchy = GetSceneHierarchy();
 
-            MethodInfo methodInfo = sceneHierarchy
+            if (sceneHierarchy == null) return null;
+
+            var methodInfo = sceneHierarchy
                 .GetType()
                 .GetMethod("GetExpandedGameObjects");
 
-            object result = methodInfo.Invoke(sceneHierarchy, new object[0]);
+            var result = methodInfo?.Invoke(sceneHierarchy, Array.Empty<object>());
 
             return (List<GameObject>) result;
         }
@@ -42,13 +50,14 @@ namespace SaG.MainSceneAutoLoading.Utilities
         /// </summary>
         public static void SetExpanded(GameObject go, bool expand)
         {
-            object sceneHierarchy = GetSceneHierarchy();
+            var sceneHierarchy = GetSceneHierarchy();
+            if (sceneHierarchy == null) return;
 
-            MethodInfo methodInfo = sceneHierarchy
+            var methodInfo = sceneHierarchy
                 .GetType()
                 .GetMethod("ExpandTreeViewItem", BindingFlags.NonPublic | BindingFlags.Instance);
 
-            methodInfo.Invoke(sceneHierarchy, new object[] {go.GetInstanceID(), expand});
+            methodInfo?.Invoke(sceneHierarchy, new object[] {go.GetInstanceID(), expand});
         }
 
         /// <summary>
@@ -56,55 +65,59 @@ namespace SaG.MainSceneAutoLoading.Utilities
         /// </summary>
         public static void SetExpandedRecursive(GameObject go, bool expand)
         {
-            object sceneHierarchy = GetSceneHierarchy();
+            var sceneHierarchy = GetSceneHierarchy();
+            if (sceneHierarchy == null) return;
 
-            MethodInfo methodInfo = sceneHierarchy
+            var methodInfo = sceneHierarchy
                 .GetType()
                 .GetMethod("SetExpandedRecursive", BindingFlags.Public | BindingFlags.Instance);
 
-            methodInfo.Invoke(sceneHierarchy, new object[] {go.GetInstanceID(), expand});
+            methodInfo?.Invoke(sceneHierarchy, new object[] {go.GetInstanceID(), expand});
         }
 
         private static object GetSceneHierarchy()
         {
-            EditorWindow window = GetHierarchyWindow();
-            object sceneHierarchy = typeof(EditorWindow).Assembly
-                .GetType("UnityEditor.SceneHierarchyWindow")
-                .GetProperty("sceneHierarchy")
-                .GetValue(window);
+            var window = GetHierarchyWindow();
 
-            return sceneHierarchy;
+            if (window == null)
+            {
+                Debug.LogWarning("Could not get scene hierarchy window.");
+                return null;
+            }
+
+            return SceneHierarchyWindowType
+                .GetProperty("sceneHierarchy")?.GetValue(window);
         }
 
         private static EditorWindow GetHierarchyWindow()
         {
-            // For it to open, so that it the current focused window.
-            EditorApplication.ExecuteMenuItem("Window/General/Hierarchy");
-            return EditorWindow.focusedWindow;
+            return EditorWindow.GetWindow(SceneHierarchyWindowType);
         }
 
         public static List<string> GetExpandedSceneNames()
         {
-            object sceneHierarchy = GetSceneHierarchy();
+            var sceneHierarchy = GetSceneHierarchy();
+            if (sceneHierarchy == null) return null;
 
-            MethodInfo methodInfo = sceneHierarchy
+            var methodInfo = sceneHierarchy
                 .GetType()
                 .GetMethod("GetExpandedSceneNames", BindingFlags.NonPublic | BindingFlags.Instance);
 
-            object result = methodInfo.Invoke(sceneHierarchy, new object[0]);
+            var result = methodInfo?.Invoke(sceneHierarchy, Array.Empty<object>());
 
             return (List<string>) result;
         }
 
         public static void SetScenesExpanded(List<string> sceneNames)
         {
-            object sceneHierarchy = GetSceneHierarchy();
+            var sceneHierarchy = GetSceneHierarchy() ;
+            if (sceneHierarchy == null) return;
 
-            MethodInfo methodInfo = sceneHierarchy
+            var methodInfo = sceneHierarchy
                 .GetType()
                 .GetMethod("SetScenesExpanded", BindingFlags.NonPublic | BindingFlags.Instance);
 
-            methodInfo.Invoke(sceneHierarchy, new object[] {sceneNames});
+            methodInfo?.Invoke(sceneHierarchy, new object[] {sceneNames});
         }
     }
 }
